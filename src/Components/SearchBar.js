9a0e5732db93a417eb1ef354e3a9a0fbf7a8a570
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-06-06 19:50:59 +0200

    initialization project

---
+++ src/Components/SearchBar.js
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./SearchBar.css";
+
+
+function SearchBar({ placeholder, data }) {
+  return (
+    <div className="search">
+      <div className="searchInputs">
+
+      </div>
+    </div>
+  );
+}
+
+export default SearchBar;
