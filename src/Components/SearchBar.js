a9c5f0c7a40e3640ca36860ee7280b92e13b1be9
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-06-06 22:08:57 +0200

    SearchBar changes

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,12 +1,25 @@
 import React, { useState } from "react";
 import "./SearchBar.css";
+import SearchIcon from "@material-ui/icons/Search";
+import CloseIcon from "@material-ui/icons/Close";
 
 
 function SearchBar({ placeholder, data }) {
+  // 1.currentState 2.updateState
+  const [filterData, setFilteredData] = useState([]);
+
+  const clearInput = () => {
+    setFilteredData([]);
+    // setWordEntered("");
+  };
   return (
     <div className="search">
+      {/* search input */}
       <div className="searchInputs">
-
+        <input type="text" placeholder="Type text"></input>
+        <div className="searchIcon">
+          {filterData.length === 0 ? (<SearchIcon/>) : (<CloseIcon id="clearBtn" onclick={clearInput} />)}
+        </div>
       </div>
     </div>
   );
