c50309afdbe0f2a09c6c55086250e530d52bd6dc
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-06-08 18:53:36 +0200

    filtering data input

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -7,6 +7,7 @@ import CloseIcon from "@material-ui/icons/Close";
 function SearchBar({ placeholder, data }) {
   // 1.currentState 2.updateState
   const [filterData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState("");
 
   const clearInput = () => {
     setFilteredData([]);
@@ -16,11 +17,26 @@ function SearchBar({ placeholder, data }) {
     <div className="search">
       {/* search input */}
       <div className="searchInputs">
-        <input type="text" placeholder="Type text"></input>
+        <input type="text" placeholder={placeholder} value={wordEntered}></input>
         <div className="searchIcon">
+          {/* if filterData empty->SearchIcon or CloseIcon */}
           {filterData.length === 0 ? (<SearchIcon/>) : (<CloseIcon id="clearBtn" onclick={clearInput} />)}
         </div>
       </div>
+      {/* if filterData!= 0 render */}
+      {filterData.length != 0 && (
+        <div className="dataReasult">
+          {filterData.slice(0, 15).map((value, key) => {
+            return (
+              <a className="dataItem" href={value.link} target="_blank">
+                <p>{ value.title }</p>
+              </a>
+            )
+          })}
+
+
+        </div>
+      )}
     </div>
   );
 }
