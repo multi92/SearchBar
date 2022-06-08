2f8e76a08502d27353461f866e9a32d4733e2bc0
Milos Mijajlovic <milos92mijajlovic@gmail.com>
2022-06-08 19:39:42 +0200

    clear button-clearINput

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -3,24 +3,48 @@ import "./SearchBar.css";
 import SearchIcon from "@material-ui/icons/Search";
 import CloseIcon from "@material-ui/icons/Close";
 
-
 function SearchBar({ placeholder, data }) {
   // 1.currentState 2.updateState
   const [filterData, setFilteredData] = useState([]);
   const [wordEntered, setWordEntered] = useState("");
 
+  const handlerFilter = (event) => {
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = data.filter((value) => {
+      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord == "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
   const clearInput = () => {
     setFilteredData([]);
-    // setWordEntered("");
+    setWordEntered("");
   };
+
+
   return (
     <div className="search">
       {/* search input */}
       <div className="searchInputs">
-        <input type="text" placeholder={placeholder} value={wordEntered}></input>
+        <input
+          type="text"
+          placeholder={placeholder}
+          value={wordEntered}
+          onChange={handlerFilter}
+        ></input>
         <div className="searchIcon">
           {/* if filterData empty->SearchIcon or CloseIcon */}
-          {filterData.length === 0 ? (<SearchIcon/>) : (<CloseIcon id="clearBtn" onclick={clearInput} />)}
+          {filterData.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id="clearBtn" onClick={clearInput} />
+          )}
         </div>
       </div>
       {/* if filterData!= 0 render */}
@@ -29,12 +53,10 @@ function SearchBar({ placeholder, data }) {
           {filterData.slice(0, 15).map((value, key) => {
             return (
               <a className="dataItem" href={value.link} target="_blank">
-                <p>{ value.title }</p>
+                <p>{value.title}</p>
               </a>
-            )
+            );
           })}
-
-
         </div>
       )}
     </div>
